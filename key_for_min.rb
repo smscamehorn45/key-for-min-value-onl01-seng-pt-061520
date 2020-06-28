68d07f79d5eeb68401466a43cd16c9146d76e07e
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-28 04:07:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,2 +1,11 @@
+names = { :blake => 500, :ashley => 2, adam => 1 }
 
-
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+      
+    end
